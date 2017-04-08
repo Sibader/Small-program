48bce6c0daf4841274d13ec579f298326a5ec52c
Sibader <296510316qq.com>
2017-04-08 13:37:08 +0800

    '小程序导航'

---
+++ small/js/hide.js
@@ -0,0 +1,160 @@
+$(function(){
+			$(".left-1").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-left-1").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-left-1").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".left-2").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-left-2").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-left-2").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".left-3").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-left-3").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-left-3").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".left-4").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-left-4").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-left-4").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".left-5").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-left-5").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-left-5").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".right-1").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-right-1").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-right-1").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".right-2").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-right-2").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-right-2").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".right-3").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-right-3").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-right-3").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".right-4").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-right-4").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-right-4").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
+$(function(){
+			$(".right-5").on({
+				mouseenter:function(){
+					console.log("over");
+					$(".Qr_code .hide-right-5").fadeIn(0,function(){
+						console.log("in");
+					});
+				},
+				mouseleave:function(){
+					console.log("out");
+					$(".Qr_code .hide-right-5").fadeOut(0,function(){
+						console.log("out");
+					});
+				}
+			});
+		});
